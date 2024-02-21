6971d560cb4eb3f9d371db5cda532d1d3fecd69e
julius <nyerereoffice@gmail.com>
2024-02-21 11:30:53 +0300

    Troisieme commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+import StarRating from "./components/star-rating";
+
+
+function App(){
+
+  return (
+    <div className="App">
+      <StarRating noOfStars={10} />
+    </div>
+  )
+}
+
+
+export default App
